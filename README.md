9d5dc470d74102f05bbe1540036e85025d5943f3
sshawarma <ssharma10393@gmail.com>
2020-09-05 19:24:04 -0500

    Switched to Hugo

--- README.md
+++
@@ -1,4 +0,0 @@
-# sshawarma.github.io
-
-This is my website for personal blogging. I talk about mathematics, computer science, and other ideas 
-here.
--- assets/js/post.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-  var links = document.getElementsByTagName('a');
-  for (var i = 0; i < links.length; i++) {
-    if (!links[i].hasAttribute('class'))
-      links[i].setAttribute('class', 'posta');
-  }
-});
